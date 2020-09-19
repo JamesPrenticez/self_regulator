4c4eaa147cb918f0ce25056b7ac30fc2ddf02e41
James-Prentice <Jamesprenticez@gmail.com>
2020-09-19 20:33:31 +1200

    Add Chicken pasta recipe

---
+++ server/data/recipes.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: 'Chicken Pasta',
+    timeToMake: '30mins',
+    servings: 4,
+    makeCount: 5,
+    ingredients: [
+        '"Chicken Thighs x 4"',
+        '"Onion x 1"',
+        '"Mushrooms"',
+        '"Bacon x 4"',
+        '"Parmesan cheese"',
+        '"Cream"',
+        '"PAMS Carbonara OR White White Sauce"',
+        '"Pasta Vegeroni"',
+        '"Salt"',
+    ],
+    intructions: [
+      '"Fry everything. 1.Chicken 2. Bacon 3. Onion 4. Mushroom"',
+      '"Boil pasta and drain.',
+      '"Put fried goods and pasta in a big pot and add 1/4 cup of cream and the whole pottle of PAMS sauce."',
+      '"Cheese and warm through in the oven for 15mins',
+    ]
+  }
+]
